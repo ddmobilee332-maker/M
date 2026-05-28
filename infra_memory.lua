460ac7278cb0cbffbe5ba60804ba2c25184bd364
Johnny_niy😝 <ddmobilee332@gmail.com>
2026-05-28 17:42:51 +0700

    Create infra_memory.lua

---
+++ infra_memory.lua
@@ -0,0 +1,11 @@
+-- NEXUS_INFRA v2.0 - Local Databank Session Storage
+-- ทำหน้าที่เก็บตัวแปรโครงสร้างแชร์ข้อมูลระหว่างคอมมานด์
+local Databank = {
+    Username = nil,
+    Password = nil,
+    TargetID = nil,
+    ZoneMap = nil,
+    IsActive = false
+}
+
+return Databank
